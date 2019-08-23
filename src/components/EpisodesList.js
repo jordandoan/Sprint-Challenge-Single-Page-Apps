3d0bb867a937c2cb16036e181538270a3ab73677
Jordan <jordandoan@hotmail.com>
2019-08-23 08:43:31 -0700

    Working on tabs

---
+++ src/components/EpisodesList.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+
+export default function EpisodesList() {
+    return <h2>Episodes</h2>
+}
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -5,12 +5,17 @@ import { NavLink } from "react-router-dom";
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
+// const panes = [
+//     { menuItem: 'Home Page', render: () => <Tab.Pane><NavLink to="/characters">Hey</NavLink></Tab.Pane> },
+//     { menuItem: 'Characters', render: () => <Tab.Pane><NavLink to="/characters"/></Tab.Pane> },
+//     { menuItem: 'Locations', render: () => <Tab.Pane><NavLink to="/locations"/></Tab.Pane> },
+//     { menuItem: 'Episodes', render: () => <Tab.Pane><NavLink to="/episodes"/></Tab.Pane> },
+//   ]
 const panes = [
-    { menuItem: 'Home Page', render: () => <Tab.Pane><NavLink to="/"/></Tab.Pane> },
-    { menuItem: 'Characters', render: () => <Tab.Pane><NavLink to="/characters"/></Tab.Pane> },
-    { menuItem: 'Locations', render: () => <Tab.Pane><NavLink to="/locations"/></Tab.Pane> },
-    { menuItem: 'Episodes', render: () => <Tab.Pane><NavLink to="/episodes"/></Tab.Pane> },
-  ]
+{ menuItem: NavLink, pane: 'Tab 1 Content' },
+{ menuItem: 'Tab 2', pane: 'Tab 2 Content' },
+{ menuItem: 'Tab 3', pane: 'Tab 3 Content' },
+];
 export default function TabNav() {
     return <Tab panes={panes}/>
 };
